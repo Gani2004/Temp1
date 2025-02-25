941984afcdf43e32b9ee00c3da1462dc2c476fc7
Gani2004 <132196673+Gani2004@users.noreply.github.com>
2025-02-25 13:21:21 +0530

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
-from Blog_Project.app.models import User
-from Blog_Project.app import db
+from app.models import User
+from app import db
 
 bp = Blueprint('auth', __name__)
 
